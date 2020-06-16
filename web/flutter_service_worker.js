7bc9b88e2669817ee8b6cecb7f19cb82646746d3
benznest <benz7net@gmail.com>
2020-06-16 23:30:31 +0700

    change readme

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -31,8 +31,8 @@ const RESOURCES = {
 "favicon.png": "d5229c38292973706b359598e9b9a23e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3db9060e285bcafa635ac7a67c498393",
-"/": "3db9060e285bcafa635ac7a67c498393",
+"index.html": "994dd69bf631e61ac584f204b522b6fb",
+"/": "994dd69bf631e61ac584f204b522b6fb",
 "main.dart.js": "07fb79591cd5bf94ff61490345643560",
 "manifest.json": "a735002b19b5b7225ea42f55122775d9"
 };
